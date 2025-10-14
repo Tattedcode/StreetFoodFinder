c24850a8d1348253977fa60dd450d13a04725aba
Liam Brown <tattedexplorer@gmail.com>
2025-10-14 16:47:52 +0800

    Initial Commit

---
+++ StreetFoodRater/StreetFoodRaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreetFoodRaterApp.swift
+//  StreetFoodRater
+//
+//  Created by Liam Brown on 14/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct StreetFoodRaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
